34343b28033903a364bcbbbfba5dbd9e97c54c8c
azra <sumeyyeazracan@gmail.com>
2026-05-27 14:57:17 +0300

    Add enterWorld function for enhanced experience

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function enter() {
   alert("Soft World activated ✨");
 }
+function enterWorld() {
+  alert("Soft Girl World aktif oldu ✨");
+
+  // küçük premium dokunuş
+  document.body.style.filter = "brightness(1.1)";
+}
